bb508aec8ab3a84baabbb442593f55c81cbd9ded
Thomas Petiteau <thomas.petiteau@outlook.com>
2022-12-12 12:10:45 +0100

    exported Window class

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from setuptools import setup, find_packages
+import setuptools
 
-setup(
+setuptools.setup(
     name="xkinter",
     version="0.0.0",
     description="python gui library built upon customtkinter",
     author="Thomas Petiteau",
     author_email="thomas.petiteau@outlook.com",
     url="https://github.com/XanX3601/xkinter",
-    packages=find_packages(),
-    install_requires = ['customtkinter==5.0.3']
+    packages=setuptools.find_packages(),
+    install_requires=["customtkinter==5.0.3"],
 )
---
+++ xkinter/Window.py
@@ -0,0 +1,9 @@
+import customtkinter
+
+
+class Window(customtkinter.CTk):
+    """Top window that is the point of entry to the application"""
+
+    def __init__(self):
+        """Instantiates this window"""
+        super().__init__()
